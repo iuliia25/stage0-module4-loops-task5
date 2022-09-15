1bdb8d392b89227171fcc327e3fa4a38cb8e285a
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-15 09:45:47 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -5,19 +5,21 @@ public class Hourglass {
         int subtract = (int) Math.floor(height/2);
         for (int i = height - subtract; i >= 1; i --) {
             for (int j = i; j < height - subtract; j ++) {
-                System.out.println(" ");
+                System.out.print(" ");
             }
             for (int j = 1; j <= i*2-1; j ++) {
-                System.out.println("8");
+                System.out.print("8");
             }
+            System.out.println();
         }
         for (int i = 2; i <= height - subtract; i ++) {
             for (int j=i; j < height- subtract; j ++) {
-                System.out.println(" ");
+                System.out.print(" ");
             }
             for (int j = 1; j <=i*2-1; j++) {
-                System.out.println("8");
+                System.out.print("8");
             }
+            System.out.println();
         }
     }
 }
